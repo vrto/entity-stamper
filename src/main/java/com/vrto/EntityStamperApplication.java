19d7e6ae35df0498c13fd184454b449647d9afce
vrto <michal.vrtiak+github@gmail.com>
2016-12-11 18:03:46 +0100

    Setting up project using a spring.io template

---
+++ src/main/java/com/vrto/EntityStamperApplication.java
@@ -0,0 +1,12 @@
+package com.vrto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityStamperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityStamperApplication.class, args);
+	}
+}
